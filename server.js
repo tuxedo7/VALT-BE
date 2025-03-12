e736a47b482059a75273f146b6d586e736af3af9
tuxedo7 <tommy.dev0127@gmail.com>
2025-03-11 21:16:19 -0700

    check cors issues

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 // app.use(express.json());
 
 app.use(express.json())
-app.use(cors());
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.urlencoded(({ extended: true })));
 // app.use("/images", express.static(path.join(__dirname, "/images")));
 
